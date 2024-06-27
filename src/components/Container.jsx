1ce6e5c4c29888f5b132e8fb81e43d3d9c940ed7
swapnils118 <swapnil118@icloud.com>
2024-06-27 00:49:04 -0400

    Complete logic with refactored code

---
+++ src/components/Container.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Stats from "./Stats";
+import Textarea from "./Textarea";
+import {
+  FACEBOOK_MAX_CHARACTERS,
+  INSTAGRAM_MAX_CHARACTERS,
+} from "../lib/constants";
+
+export default function Container() {
+  const [text, setText] = useState("");
+  //Refactoring
+  // const numberOfWords = text.split(/\s/).filter((word) => word !== "").length;
+  // const numberOfCharacters = text.length;
+  // const instagramCharactersLeft = 280 - text.length;
+  // const facebookCharactersLeft = 2200 - text.length;
+  const stats = {
+    numberOfWords: text.split(/\s/).filter((word) => word !== "").length,
+    numberOfCharacters: text.length,
+    instagramCharactersLeft: INSTAGRAM_MAX_CHARACTERS - text.length,
+    facebookCharactersLeft: FACEBOOK_MAX_CHARACTERS - text.length,
+  };
+
+  return (
+    <main className="container">
+      <Textarea text={text} setText={setText} />
+      <Stats stats={stats} />
+    </main>
+  );
+}
